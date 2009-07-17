4f0c4c9f5324d039106e1bcd9183c5f8f97b06e7
Shane <enginzhu@vip.qq.com>
2009-07-17 22:16:18 +0800

    set Environment Variable

--- setSysEnvir.py
+++ setSysEnvir.py
@@ -2,21 +2,24 @@
 
 import win32api
 import win32con
+import os
 
-def setSysEnvir(Var_Name,Var_Value):
+def setSysEnvir(Var_Value):
     """set the windows system environment. """
     
     # open the position where system stores system environment.
     pathInReg = 'SYSTEM\\CurrentControlSet\\Control\\Session Manager\\Environment'
+    key = win32api.RegOpenKey(win32con.HKEY_LOCAL_MACHINE, pathInReg, 0, win32con.KEY_ALL_ACCESS)
     
-    key = win32api.RegOpenKey(win32con.HKEY_LOCAL_MACHINE, \
-                              pathInReg, 0, win32con.KEY_ALL_ACCESS)
-    
-    win32api.RegSetValueEx(key, Var_Name, 0, win32con.REG_SZ, Var_Value)
+    #先读取path的值，追加路径后再覆盖原来的path。
+    value = win32api.RegQueryValueEx(key, 'path')
+    newValue = value[0] + ";" + Var_Value
+    win32api.RegSetValueEx(key, 'path', 0, win32con.REG_SZ, newValue)
     win32api.RegCloseKey(key)
 
 if __name__ == "__main__":
-    setSysEnvir('Shane','Chu')
+    setSysEnvir('E:\\testdir')
     print 'great!'
 
-'''可能不会立即生效，不然得重启'''
+#不会立即生效，需要重启
+
