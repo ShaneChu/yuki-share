a7e98d5e1ad554bbe80f82d6df5f43f6f784db6a
ShaneChu <enginzhu@vip.qq.com>
2009-09-11 01:43:20 +0800

    Yuki-Share

--- cli_release.py
+++ cli_release.py
@@ -32,19 +32,31 @@ class mythread(threading.Thread):
         return self.res
     
 class fileShare:
-    
+
+    def __init__(self):
+
+        self.localName = socket.gethostname()
+        #get the localhost IP address throught connecting Google Server
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(('google.com', 0))
+        self.local_IP = s.getsockname()[0]
+        self.neighbor_list = []
+        
+        cur = os.path.dirname(os.path.realpath(__file__))
+        self.directory = os.path.dirname(os.path.dirname(cur))
+        
+
     def HTTPServer(self):
         try:
             '''Share the current directory, start the http server'''
             os.chdir(self.directory)     #switch to the share directory
             handler = SimpleHTTPServer.SimpleHTTPRequestHandler
             httpd = SocketServer.TCPServer((self.local_IP, 8800), handler)
-            print '\tService start successful...'
+            print '\tService start successfully...'
             print "\thttp Server URL: http://" + self.local_IP + ':8800'
             httpd.serve_forever()
         except:
-            print '\tError:cannot startup.please make sure that the dir is right.\
-                   \t      Others, the service couldnot startup twice.'
+            print '\tError : The service couldnot startup twice.'
     
     
     def HTTP_svr_Thread(self):
@@ -78,8 +90,8 @@ class fileShare:
                         pass
                 except:
                     self.neighbor_list.append(dict)
-                    len = self.neighbor_list.__len__() - 1      #add the last online user 
-                    print (self.neighbor_list[len])['IP'] + ':8800' + '\t\t' + (self.neighbor_list[len])['share_dir_list']
+                    #len = self.neighbor_list.__len__() - 1      #add the last online user 
+                    #print (self.neighbor_list[len])['IP'] + ':8800' + '\t\t' + (self.neighbor_list[len])['share_dir_list']
                 
                 s.sendto(self.directory, address)    #send a reply.
 
@@ -92,8 +104,8 @@ class fileShare:
         
     def boardcast(self, widget=None):
         #send a boardcast message for renewing the list
-        self.neighbor_list = []     #initialize the list
-        #self.neighbor_list = [{'192.168.1.102':'/windows/sda5'}, {'192.168.1.103':'/windows/sda7'}]
+        #self.neighbor_list = []     #initialize the list
+        self.neighbor_list = [{'192.168.1.102':'/windows/sda5'}, {'192.168.1.103':'/windows/sda7'}]
         dest = ("<broadcast>",50000)    #boardcast address
         self.reply_socket.sendto(self.directory, dest)
         
@@ -116,8 +128,8 @@ class fileShare:
                     pass
             except:
                 self.neighbor_list.append(dict)
-                len = self.neighbor_list.__len__() - 1
-                print  (self.neighbor_list[len])['IP'] + ':8800' + '\t\t' + (self.neighbor_list[len])['share_dir_list']
+                #len = self.neighbor_list.__len__() - 1
+                #print  (self.neighbor_list[len])['IP'] + ':8800' + '\t\t' + (self.neighbor_list[len])['share_dir_list']
     
     def reply_thread(self):
         reply = mythread(self.replyReceive)
@@ -126,80 +138,71 @@ class fileShare:
     
     
     def main(self):
-            #obtain the localhost name
-            self.localName = socket.gethostname()
-            #get the localhost IP address throught connecting Google Server
-            s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-            s.connect(('google.com', 0))
-            self.local_IP = s.getsockname()[0]
-            #backup the share directory through a global variable
-            if not os.path.exists('FileShare.cfg'):
-                print 'config file does not exist...fail to share...'
-            else:
-                file = open('FileShare.cfg', 'r')
-                self.directory = file.read()
-                file.close()
-                
-            while 1:
-                str = raw_input('Yuki-Share>')
-                if str == 'help':
-                    print 'yuki-share.py : the command_line release of the Yuki-Share system\n'
-                    print 'Usage:\n\tYuki-Share> [options]'
-                    print 'Options: '
-                    print '\thelp     --get more help about Yuki-Share'
-                    print '\tinfo     --show the host information'
-                    print '\tstart    --start the share service'
-                    print '\tisstart  --check out whether the sevice was start or not'
-                    print '\tlist     --print the online users and their sharing'
-                    print '\topen     --open a page for browsing the neighbor\'s sharing file'
-                    print '\t         --Usage: open [IP Address]\n\t         --For example: open 192.168.1.100'
-                    print '\texit     --exit'
-                    print ''
-                elif str == 'info':
-                    print '\thost Name:', self.localName
-                    print '\toperate system:', os.sys.platform
-                    print '\tlocalhost IP:', self.local_IP
-                    print '\tlocalhost Share Directory:', self.directory
-                elif str == 'start':
-                    self.HTTP_svr_Thread()
-                    self.svr_thread()
-                    self.reply_thread()
-                    time.sleep(0.1)
-                elif str == 'isstart':
-                    #the config file shouldn't exist since the current dir has changed
-                    if not os.path.exists('FileShare.cfg'):
-                        print '\tService has been started'
-                    else:
-                        print '\tService has not been started'
-                elif str == 'list':
-                    try:
-                        self.boardcast()
-                    except:
-                        print 'Error: Your program has not been started'
-                        continue
-                    time.sleep(1)
-                    if self.neighbor_list == []:
-                        print '\tResult: There is no any other users online now.'
-                    else:
-                        print '\t  LAN Host' + '\t\t' + '  Share Dir'
-                        for user in self.neighbor_list:
-                            print '\t' + user.keys()[0] + '\t\t' + user.values()[0]
-                elif str.find('open') != -1:
-                    array = str.split()
-                    if array[0] == 'open':
-                        if len(array) == 2:
-                            check = re.match(r"^[\d]*[\d|.]*[\d]$", array[1])
-                            if check != None:
-                                webbrowser.open_new_tab('http://' + array[1] + ':8800')
-                            else:
-                                print '\tInvalid Usage.\tTry: open [IP Address]'
-                        else:
-                            print '\tInvalid Usage.\tTry: open [IP Address]'
-                elif str == 'exit':
-                    print '\tThank you for using Yuki-Share.'
-                    sys.exit()
-                else:
-                    print '\tFor more help or getting more Usages, Try input \'help\' '
+
+        try:
+	        while 1:
+	            str = raw_input('Yuki-Share>')
+	            if str == 'help':
+	                print 'yuki-share.py : the command_line release of the Yuki-Share system\n'
+	                print 'Usage:\n\tYuki-Share> [options]'
+	                print 'Options: '
+	                print '\thelp     --get more help about Yuki-Share'
+	                print '\tinfo     --show the host information'
+	                print '\tstart    --start the share service'
+	                print '\tisstart  --check out whether the sevice was start or not'
+	                print '\tlist     --print the online users and their sharing'
+	                print '\topen     --open a page for browsing the neighbor\'s sharing file'
+	                print '\t         --Usage: open [IP Address]\n\t         --For example: open 192.168.1.100'
+	                print '\texit     --exit'
+	                print ''
+	            elif str == 'info':
+	                print '\thost Name:', self.localName
+	                print '\toperate system:', os.sys.platform
+	                print '\tlocalhost IP:', self.local_IP
+	                print '\tlocalhost Share Directory:', self.directory
+	            elif str == 'start':
+	                self.HTTP_svr_Thread()
+	                self.svr_thread()
+	                self.reply_thread()
+	                time.sleep(0.1)
+	            elif str == 'isstart':
+	                #the file shouldn't exist since the current dir has changed
+	                if not os.path.exists('cli_release.py'):
+	                    print '\tService has been started'
+	                else:
+	                    print '\tService has not been started'
+	            elif str == 'list':
+	                try:
+	                    self.boardcast()
+	                except:
+	                    print 'Error: Your program has not been started'
+	                    continue
+	                time.sleep(1)
+	                if self.neighbor_list == []:
+	                    print '\tResult: There is no any other users online now.'
+	                else:
+	                    print '\t  LAN Host' + '\t\t' + '  Share Dir'
+	                    for user in self.neighbor_list:
+	                        print '\t' + user.keys()[0] + '\t\t' + user.values()[0]
+	            elif str.find('open') != -1:
+	                array = str.split()
+	                if array[0] == 'open':
+	                    if len(array) == 2:
+	                        check = re.match(r"^[\d]*[\d|.]*[\d]$", array[1])
+	                        if check != None:
+	                            webbrowser.open_new_tab('http://' + array[1] + ':8800')
+	                        else:
+	                            print '\tInvalid Usage.\tTry: open [IP Address]'
+	                    else:
+	                        print '\tInvalid Usage.\tTry: open [IP Address]'
+	            elif str == 'exit':
+	                print '\tThank you for using Yuki-Share.'
+	                sys.exit()
+	            else:
+	                print '\tFor more help or getting more Usages, Try input \'help\' '
+        except KeyboardInterrupt:
+            print '\n\tThank you for using Yuki-Share.'
+            sys.exit()
                     
     
 if __name__ == '__main__':
---
+++ gui_release.py
@@ -0,0 +1,113 @@
+#! usr/bin/python
+# -*- coding:utf-8 -*-
+# Filename: gui_release.py
+
+from cli_release import fileShare
+
+
+import re
+import os
+import sys
+import time
+import socket
+import thread
+import threading
+import webbrowser
+import gtk, pygtk
+pygtk.require('2.0')
+
+
+class GYuki(gtk.Window):
+
+    def __init__(self):
+
+        gtk.Window.__init__(self)
+        self.set_title('Yuki-Share')
+        self.connect('destroy', gtk.main_quit)
+        self.set_size_request(300, 300)
+        self.set_position(gtk.WIN_POS_CENTER)
+
+        # These four lines below should be added to the __init__() function in GUI
+        self.fshare = fileShare()
+        self.fshare.HTTP_svr_Thread()
+        self.fshare.svr_thread()
+        self.fshare.reply_thread()
+        time.sleep(0.05)
+		
+        self.table = gtk.Table(20, 20, True)
+        self.add(self.table)
+        self.addrEntry = gtk.combo_box_entry_new_text()
+        self.table.attach(self.addrEntry, 0, 15, 0, 2)
+
+        # put the online users into pull-down lists
+        for user in self.fshare.neighbor_list:
+            ip = user.keys()[0]
+            self.addrEntry.append_text(ip)
+
+        self.open = gtk.Button('Open')
+        self.table.attach(self.open, 15, 20, 0, 2)
+        self.open.connect('clicked',self.openWeb)
+
+        self.onLab = gtk.Label('Online Users :')
+        self.table.attach(self.onLab, 0, 7, 2, 4)
+        self.renew = gtk.Button('Renew')
+        self.table.attach(self.renew, 15, 20, 2, 4)
+        self.renew.connect('clicked', self.renewUser)
+
+        self.swindow = gtk.ScrolledWindow()
+        self.text = gtk.TextView()
+        self.text.set_editable(False)
+        self.textbuffer = self.text.get_buffer()
+        self.swindow.add(self.text)
+        self.table.attach(self.swindow, 0, 20, 4, 14)
+
+        if self.fshare.neighbor_list != []:
+            self.textbuffer.set_text('neighbor users' + '\t\tdirectory\n')
+            for user in self.fshare.neighbor_list:
+                ip = user.keys()[0]
+                dir = user.values()[0]
+                iter = self.textbuffer.get_end_iter()
+                self.textbuffer.insert(iter, ip + '\t\t' + dir + '\n')
+        else:
+            self.textbuffer.set_text('There is no any other users online now.')
+        
+        self.infoLab = gtk.Label('Hostname : '+self.fshare.localName)
+        self.infoLab.set_text(self.infoLab.get_text()+'\nOperate System : '+os.sys.platform)
+        self.infoLab.set_text(self.infoLab.get_text()+'\nLocalhost IP : '+self.fshare.local_IP)
+        self.infoLab.set_text(self.infoLab.get_text()+'\nShare Dir : '+self.fshare.directory)
+        self.table.attach(self.infoLab, 0, 18, 14, 20)
+
+        self.show_all()
+
+
+    def openWeb(self, widget=None):
+
+        ip = self.addrEntry.get_active_text()
+        check = re.match(r"^[\d]*[.][\d]*[.][\d]*[.][\d]*$", ip)
+
+        if check != None:
+            url = 'http://' + ip + ':8800'
+            webbrowser.open_new_tab(url)
+        else:
+            self.warning('Invalid Address Format')
+
+
+    def renewUser(self, widget=None):
+        self.fshare.boardcast()
+
+
+    def warning(self, message):
+        mydialog = gtk.MessageDialog(None, 0, gtk.MESSAGE_WARNING, gtk.BUTTONS_OK, message)
+        t = mydialog.run()
+        mydialog.destroy()
+
+
+if __name__ == '__main__':
+    gtk.gdk.threads_init()
+    gyuki = GYuki()
+    gtk.gdk.threads_enter()
+    gtk.main()
+    gtk.gdk.threads_leave()
+
+
+
